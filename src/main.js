16072813e3149e2bfa899c1871d4acaa39c45d1c
Thomas Kwak <teekwak@gmail.com>
2017-07-06 14:26:11 -0700

    Added jQuery and Bootstrap to entry point

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
+require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap/dist/js/bootstrap.min.js');
+require('jquery/dist/jquery.min.js');
+
 ReactDOM.render(
   <Routes />,
   document.getElementById('app')
