ab542cec1f8ec5497a8c0848fd50630db6da77ef
Thomas Kwak <teekwak@gmail.com>
2017-07-06 14:15:46 -0700

    Cleaned up unused code from main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// require('bootstrap/dist/css/bootstrap.min.css');
-// require('bootstrap/dist/js/bootstrap.min.js');
-// require('jquery/dist/jquery.min.js');
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   ReactDOM.render(
-//     React.createElement(App),
-//     document.getElementById('app')
-//   );
-// });
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
