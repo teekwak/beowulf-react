47df0bc2cfe4a744adc7d1a058365bb96ae60d12
Thomas Kwak <teekwak@gmail.com>
2017-07-06 15:41:48 -0700

    Cleaned up App.js to render splash page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Counter from './components/Counter';
 import NavigationBar from './components/NavigationBar';
-import StatusCheckerContainer from './containers/StatusCheckerContainer';
 
-// crap
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const NotFound = () => (
-  <div>
-    <h2>NotFound</h2>
-  </div>
-);
-
-/**
- * Entry point
- * Note: all routing happens here and NOT by Express
- * TODO: if I go directly to /about, the page will fail (probably expecting Express to handle)
- */
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello world
+        <NavigationBar highlighted="home" />
+        Home
       </div>
     );
   }
 }
 
-App.Home = Home;
-App.About = About;
-App.NotFound = NotFound;
-
 export default App;
-
